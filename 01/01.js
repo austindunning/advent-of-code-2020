ca08897ae2f15dd47209a1b575e3ff237d30356f
austindunning <dunning.austin@gmail.com>
2020-12-01 21:38:52 -0500

    Day 1

---
+++ 01/01.js
@@ -0,0 +1,17 @@
+input = require('./input').getInput()
+    .split('\n')
+    .map(Number);
+
+let values = {};
+
+for (var i = 0; i < input.length; i++) {
+    values[input[i]] = i;
+}
+
+for (var i = 0; i < input.length; i++) {
+    let difference = 2020 - input[i];
+    if (values.hasOwnProperty(difference) && values[difference] !== i) {
+        console.log(input[i] * difference);
+        return;
+    }
+}
---
+++ 01/02.js
@@ -0,0 +1,14 @@
+input = require('./input').getInput()
+    .split('\n')
+    .map(Number);
+
+for (var i = 0; i < input.length; i++) {
+    for (var j = i + 1; j < input.length; j++) {
+        for (var k = j + 1; k < input.length; k++) {
+            if (input[i] + input[j] + input[k] === 2020) {
+                console.log(input[i] * input[j] * input[k]);
+                return;
+            }
+        }
+    }
+}
