6d826eceb7f2bd1df475b2eccf71e7961f19411f
austindunning <dunning.austin@gmail.com>
2020-12-05 11:49:15 -0500

    Day 5 part 1

---
+++ 05/1.js
@@ -0,0 +1,21 @@
+input = require('./input').getInput();
+
+const getRowOrColumn = (arr, instructions) => {
+  instructions.forEach((x) => {
+    if (['B', 'R'].includes(x)) {
+      arr.splice(0, arr.length / 2);
+    } else {
+      arr.splice(arr.length / 2);
+    }
+  });
+  return arr[0];
+};
+
+const seatIds = input.map((x) => {
+  x = x.split('');
+  const row = getRowOrColumn([...Array(128).keys()], x.splice(0, 7));
+  const column = getRowOrColumn([...Array(8).keys()], x);
+  return row * 8 + column;
+});
+
+console.log(Math.max(...seatIds));
