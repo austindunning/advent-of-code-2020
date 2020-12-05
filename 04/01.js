1ead3d03d153167486ad68bf0638e3824333748e
Austin Dunning <dunning.austin@gmail.com>
2020-12-05 00:34:35 -0500

    Prettier

--- 04/01.js
+++ 04/01.js
@@ -2,9 +2,7 @@ input = require('./input').getInput();
 
 const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 
-const passports = input.map((x) =>
-  [...x.matchAll(/(\w+):/g)].map((y) => y[1])
-);
+const passports = input.map((x) => [...x.matchAll(/(\w+):/g)].map((y) => y[1]));
 
 const isValid = (passportFields, requiredFields) =>
   requiredFields.every((x) => passportFields.includes(x));
